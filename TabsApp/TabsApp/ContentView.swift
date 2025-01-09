d12e0018fcffaf2c3de772d5f3b658a2921cb088
Efren Reyes <efrenreyes@iMac-de-Efren.local>
2025-01-08 19:35:28 -0600

    Creacion de Proyecto TabsApp

---
+++ TabsApp/TabsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabsApp
+//
+//  Created by Efren Reyes on 08/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabsApp/TabsApp/TabsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabsAppApp.swift
+//  TabsApp
+//
+//  Created by Efren Reyes on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TabsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
