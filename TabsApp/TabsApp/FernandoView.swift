2887bbc8cba807f8dda7f57bb54f77bbff95e577
Daniela Cabrera <danielacabrera@MacBook-Pro-de-Daniela.local>
2025-01-08 20:19:55 -0600

    un cambio en vista FernandoView

--- TabsApp/TabsApp/FernandoView.swift
+++ TabsApp/TabsApp/FernandoView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct FernandoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+           Text("¿Que paso?")
+        }
     }
 }
 
